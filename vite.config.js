a5c9d7c49cfef84aa86ac6161b0ef319189c9cea
Jc-0423 <jc0981606985@gmail.com>
2024-07-16 14:01:52 +0800

    fix:/修正顯示問題

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
-  base: '/',
+  base: 'ShoppingCart-Vue-Tailwind',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
